5814e0954e99d3b4c41c250900b4da498e2ae700
kot-pilun <kotpilun3m@gmail.com>
2023-01-09 12:15:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const stringDig = n >= 0 ? String(n) : String(-1 * n);
+    let revDig = '';
+    for (let i = 0; i <= stringDig.length-1; i++) {
+        revDig = stringDig[i] + revDig;
+    }
+    return Number(revDig);
 }
